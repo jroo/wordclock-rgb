e52411509325c886e2cf73d245c6a8b7293211bd
jroo <ruihley@gmail.com>
2015-11-07 11:03:39 -0500

    add brightness, refactor for memory efficiency

--- WordClockRGB/WordClockRGB.ino
+++ WordClockRGB/WordClockRGB.ino
@@ -1,46 +1,28 @@
-/***************************************************************************
- *                                                                         *
- *  W O R D C L O C K R G B   - A clock that tells the time using words.   *
- *                                                                         *
- * Hardware: Arduino with a set of individual LEDs under a                 *
- * word stencil.                                                           *
- *                                                                         *
- *                                                                         *
- *   Original Copyright (C) 2009  Doug Jackson (doug@doughq.com)           *
- *   Modifications Copyright (C) 2010 Scott Bezek (scott@bezekhome.com)    *
- *   Modifications Copyright (C) 2015 Joshua Ruihley                       *
- *                                                                         *
- ***************************************************************************
- *                                                                         *
- *   This program is free software: you can redistribute it and/or modify  *
- *    it under the terms of the GNU General Public License as published by *
- *    the Free Software Foundation, either version 3 of the License, or    *
- *    (at your option) any later version.                                  *
- *                                                                         *
- *    This program is distributed in the hope that it will be useful,      *
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- *    GNU General Public License for more details.                         *
- *                                                                         *
- *    You should have received a copy of the GNU General Public License    *
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.*
- *                                                                         *
- ***************************************************************************
- * 
- * Revision History
- * 
- * Date         By          What
- * 20001025     DRJ         Initial Creation of Arduino Version 
- *                              - based on Wordclock.c - from PIC version
- * 20100124     Scott Bezek Changed LED pinout, added brightness control,
- *                              changed buttons to hour/minute increment 
- * 20150802     JR          added four LEDs for minute display, changed
- *                          brightness setting trigger to a button, added
- *                          "IT IS" and removed "Minutes" from display.
- */
- 
+/*
+* RGB Word Clock
+* https://github.com/jroo/wordclock-rgb/
+* 
+* Modified from the following:
+*   - 2009: Doug Jackson, http://www.instructables.com/id/The-Wordclock-Grew-Up/
+*   - 2010: Scott Bezek, http://scottbezek.blogspot.com/2010/07/my-first-instructable.html
+*   - 2015: Josh Ruihley, https://github.com/jroo/wordclock
+*
+* This program is free software: you can redistribute it and/or modify  
+* it under the terms of the GNU General Public License as published by 
+* the Free Software Foundation, either version 3 of the License, or    
+* (at your option) any later version.                                  
+*
+* This program is distributed in the hope that it will be useful,      
+* but WITHOUT ANY WARRANTY; without even the implied warranty of       
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+* GNU General Public License for more details.                         
+*                                                                         
+* You should have received a copy of the GNU General Public License    
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+*/
+
 #include <Time.h>  
-#include <Wire.h>  
 #include <DS1307RTC.h> 
 #include <Adafruit_NeoPixel.h>
 
@@ -59,85 +41,90 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(256, 6, NEO_GRB + NEO_KHZ800);
 
 struct coords {
-  int y;
-  int x;
+  byte y;
+  byte x;
 };
 
+struct word_info {
+  String title;
+  byte len;
+  coords pos[9];
+};
+
+#define ITIS    {F("IT IS"), 4, { {3,2}, {3,3}, {3,5}, {3,6} } }
+#define QUARTER {F("A QUARTER"), 9, { {4,2}, {4,4}, {4,5}, {4,6}, {4,7}, {4,8}, {4,9}, {4,10} } }
+#define TWENTY  {F("TWENTY"), 6, { {5,2}, {5,3}, {5,4}, {5,5}, {5,6}, {5,7} } }
+#define MFIVE   {F("FIVE"), 4, { {5,8}, {5,9}, {5,10}, {5,11} } }
+#define HALF    {F("HALF") , 4, { {6,2}, {6,3}, {6,4}, {6,5} } }
+#define MTEN    {F("TEN"), 3, { {6,7}, {6,8}, {6,9} } }
+#define TO      {F("TO"), 2, { {6,11}, {6,12} } }
+#define PAST    {F("PAST"), 4, { {7,2}, {7,3}, {7,4}, {7,5} } }
+#define NINE    {F("NINE"), 4, { {7,9}, {7,10}, {7,11}, {7,12} } }
+#define ONE     {F("ONE"), 3, { {8,2}, {8,3}, {8,4} } }
+#define SIX     {F("SIX"), 3, { {8,5}, {8,6}, {8,7} } }
+#define THREE   {F("THREE"), 5, { {8,8}, {8,9}, {8,10}, {8,11}, {8,12} } }
+#define FOUR    {F("FOUR"), 4, { {9,2}, {9,3}, {9,4}, {9,5} } }
+#define HFIVE   {F("FIVE"), 4, { {9,6}, {9,7}, {9,8}, {9,9} } }
+#define TWO     {F("TWO"), 3, { {9,10}, {9,11}, {9,12} } }
+#define EIGHT   {F("EIGHT"), 5, { {10,2}, {10,3}, {10,4}, {10,5}, {10,6} } }
+#define ELEVEN  {F("ELEVEN"), 6, { {10,7}, {10,8}, {10,9}, {10,10}, {10,11}, {10,12} } }
+#define SEVEN   {F("SEVEN"), 5, { {11,2}, {11,3}, {11,4}, {11,5}, {11,6} }}
+#define TWELVE  {F("TWELVE"), 6, { {11,7}, {11,8}, {11,9}, {11,10}, {11,11}, {11,12} } }
+#define HTEN    {F("TEN"), 3, { {12,2}, {12,3}, {12,4} } }
+#define OCLOCK  {F("OCLOCK"), 5, { {12,7}, {12,8}, {12,9}, {12,10}, {12,11}, {12,12} } }
+#define MIN1    {F("."), 1, { {0,0} } }
+#define MIN2    {F("."), 1, { {0,15} } }
+#define MIN3    {F("."), 1, { {15,0} } }
+#define MIN4    {F("."), 1, { {15,15} } }
 
 #define NUMROWS 16
 #define NUMCOLS 16
-
-coords ITIS[] = {{2,2},{2,3},{2,5},{2,6}};
-coords QUARTER[] = {{3,2},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9},{3,10}};
-coords TWENTY[] =  {{4,2}, {4,3}, {4,4}, {4,5}, {4,6}, {4,7}};
-coords MFIVE[] = {{4,8}, {4,9}, {4,10}, {4,11}};
-coords HALF[] = {{5,2}, {5,3}, {5,4}, {5,5}};
-coords MTEN[] = {{5,7}, {5,8}, {5,9}};
-coords TO[] = {{5,11}, {5,12}}; 
-coords PAST[] = {{6,2}, {6,3}, {6,4}, {6,5}}; //   {int 6,[2,3,4,5]}
-coords NINE[] = {{6,9}, {6,10}, {6,11}, {6,12}}; //    {int 6,[9,10,11,12]}
-coords ONE[] = {{7,2}, {7,3}, {7,4}}; //     {int 7,[2,3,4]}
-coords SIX[] = {{7,5}, {7,6}, {7,7}}; //     {int 7,[5,6,7]}
-coords THREE[] = {{7,8}, {7,9}, {7,10}, {7,11}, {7,12}}; //   {int 7,[8,9,10,11,12]}
-coords FOUR[] = {{8,2}, {8,3}, {8,4}, {8,5}}; //    {int 8,[2,3,4,5]}
-coords HFIVE[] = {{8,6}, {8,7}, {8,8}, {8,9}}; //   {int 8,[6,7,8,9]}
-coords TWO[] = {{8,10}, {8,11}, {8,12}}; //     {int 8,[10,11,12]}
-coords EIGHT[] = {{9,2}, {9,3}, {9,4}, {9,5}, {9,6}}; //   {int 9,[2,3,4,5,6]}
-coords ELEVEN[] = {{9,7}, {9,8}, {9,9}, {9,10}, {9,11}, {9,12}}; //  {int 9,[7,8,9,10,11,12]}
-coords SEVEN[] = {{10,2}, {10,3}, {10,4}, {10,5}, {10,6}}; //   {int 10,[2,3,4,5,6]}
-coords TWELVE[] = {{10,7}, {10,8}, {10,9}, {10,10}, {10,11}, {10,12}}; //  {int 10,[7,8,9,10,11,12]}
-coords HTEN[] = {{11,2}, {11,3}, {11,4}}; //    {int 11,[2,3,4]}
-coords OCLOCK[] = {{11,7}, {11,8}, {11,9}, {11,10}, {11,11}, {11,12}}; //  {int 11,[7,8,9,10,11,12]}
-coords MIN1[] = {{0,0}}; //    {int 1, [1]}
-coords MIN2[] = {{0,15}}; //    {int 15, [1]}
-coords MIN3[] = {{15,0}}; //    {int 15, [15]}
-coords MIN4[] = {{15,15}}; //    {int 1, [15]}
+#define MAX_BRIGHTNESS 64 //do not increase unless you're absolutely sure you have the proper power supply to drive it
 
 static unsigned long msTick =0;  // the number of Millisecond Ticks since we last 
                                  // incremented the second counter
 
-// hardware constants
-int HourButtonPin=4;
-int hourButtonDown = 0;
 
-int MinuteButtonPin=5;
-int minuteButtonDown = 0;
+byte BrightnessPin = 3;
+byte brightnessButtonDown = 0;
+byte brightness = MAX_BRIGHTNESS;
 
-int brightness = 32;
+byte HourPin = 4;
+byte hourButtonDown = 0;
 
-int pushStart = 0;
+byte MinutePin = 5;
+byte minuteButtonDown = 0;
+
+byte pushStart = 0;
 int longPressDelay = 400; //time in millisecnods considered to be a long press of a button
-int inLongPress = 0; //true if long press is happening
+byte inLongPress = 0; //true if long press is happening
 
 time_t t;
-
 
 void setup()
 {
   // initialize the hardware	
-  pinMode(MinuteButtonPin, INPUT); 
-  pinMode(HourButtonPin, INPUT);
+  pinMode(MinutePin, INPUT); 
+  pinMode(HourPin, INPUT);
   
-  digitalWrite(MinuteButtonPin, HIGH);  //set internal pullup
-  digitalWrite(HourButtonPin, HIGH); //set internal pullup
+  digitalWrite(MinutePin, HIGH);  //set internal pullup
+  digitalWrite(HourPin, HIGH); //set internal pullup
 
   //initialize system time from rtc
   Serial.begin(9600);
-  Serial.println("getting time from RTC");
+  Serial.println(F("Getting time from RTC"));
   setTime(RTC.get());   // the function to get the time from the RTC
-  Serial.println("moving on");
+  Serial.println(F("Moving on"));
   msTick=millis();      // Initialise the msTick counter
   displaytime();        // display the current time
 
   strip.begin();
-  strip.setBrightness(brightness);
+  strip.setBrightness(MAX_BRIGHTNESS);
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop(void)
-{  
-  //selftest(); //uncomment to run in test mode
-  
+{    
     // heart of the timer - keep looking at the millisecond timer on the Arduino
     // and increment the seconds counter every 1000 ms
     if ( millis() - msTick >999) {
@@ -158,7 +145,7 @@ void loop(void)
     //check to see if buttons are being pressed
     checkHourButton();
     checkMinuteButton();
-    //checkBrightnessButton();
+    checkBrightnessButton();
 }
 
 void ledsoff(void) {
@@ -190,94 +177,55 @@ int getIndex(int y, int x) {
   return index;
 }
 
-void DisplayWord(coords word_info[], String timeword) {
+void DisplayWord(word_info cword) {
   // Now we write the actual values to the hardware
-  for (int i=0; i<sizeof(word_info); i++) {
-    strip.setPixelColor(getIndex(word_info[i].y, word_info[i].x), strip.Color(128, 0, 0));
+  Serial.print(cword.title);
+  Serial.print(F(" "));
+  for (int i=0; i<cword.len; i++) {
+    strip.setPixelColor(getIndex(cword.pos[i].y, cword.pos[i].x), strip.Color(128, 128, 128));
   }
 }
-
-/*
-void selftest(void){
-  //cycle through each word and display it
-  Serial.print("TEST");
-
-  ledsoff(); ITIS; MTEN; HALF; QUARTER; TWENTY; MFIVE; PAST; TO; ONE; TWO; THREE; FOUR; HFIVE; SIX; SEVEN; EIGHT; NINE; HTEN; ELEVEN; TWELVE; OCLOCK; MIN1; MIN2; MIN3; MIN4; WriteLEDs(); delay(2000); 
-  
-  ledsoff(); ITIS; WriteLEDs(); delay(1000); 
-  ledsoff(); MTEN; WriteLEDs(); delay(1000); 
-  ledsoff(); HALF; WriteLEDs(); delay(1000); 
-  ledsoff(); QUARTER; WriteLEDs(); delay(1000); 
-  ledsoff(); TWENTY; WriteLEDs(); delay(1000); 
-  ledsoff(); MFIVE; WriteLEDs(); delay(1000); 
-  ledsoff(); PAST; WriteLEDs(); delay(1000); 
-  ledsoff(); TO; WriteLEDs(); delay(1000); 
-  ledsoff(); ONE; WriteLEDs(); delay(1000); 
-  ledsoff(); TWO; WriteLEDs(); delay(1000); 
-  ledsoff(); THREE; WriteLEDs(); delay(1000); 
-  ledsoff(); FOUR; WriteLEDs(); delay(1000); 
-  ledsoff(); HFIVE; WriteLEDs(); delay(1000); 
-  ledsoff(); SIX; WriteLEDs(); delay(1000); 
-  ledsoff(); SEVEN; WriteLEDs(); delay(1000); 
-  ledsoff(); EIGHT; WriteLEDs(); delay(1000); 
-  ledsoff(); NINE; WriteLEDs(); delay(1000); 
-  ledsoff(); HTEN; WriteLEDs(); delay(1000); 
-  ledsoff(); ELEVEN; WriteLEDs(); delay(1000); 
-  ledsoff(); TWELVE; WriteLEDs(); delay(1000); 
-  ledsoff(); OCLOCK; WriteLEDs(); delay(1000);
-  ledsoff(); MIN1; WriteLEDs(); delay(1000); 
-  ledsoff(); MIN2; WriteLEDs(); delay(1000); 
-  ledsoff(); MIN3; WriteLEDs(); delay(1000); 
-  ledsoff(); MIN4; WriteLEDs(); delay(1000);
-
-  ledsoff(); MIN1; WriteLEDs(); Serial.println("MIN1"); delay(1000); 
-  ledsoff(); MIN2; WriteLEDs(); Serial.println("MIN2"); delay(1000); 
-  ledsoff(); MIN3; WriteLEDs(); Serial.println("MIN3"); delay(1000); 
-  ledsoff(); MIN4; WriteLEDs(); Serial.println("MIN4"); delay(1000);
-  ledsoff(); delay(1000);
- }
- */
 
 void displayHour(int offset) {
       switch (hourFormat12()+offset) {
         case 1: 
-          DisplayWord(ONE, "One");
+          DisplayWord(ONE);
           break;
         case 2: 
-          DisplayWord(TWO, "Two");
+          DisplayWord(TWO);
           break;
         case 3: 
-          DisplayWord(THREE, "Three");
+          DisplayWord(THREE);
           break;
         case 4: 
-          DisplayWord(FOUR, "Four");
+          DisplayWord(FOUR);
           break;
         case 5: 
-          DisplayWord(HFIVE, "Five"); 
+          DisplayWord(HFIVE); 
           break;
         case 6: 
-          DisplayWord(SIX, "Six");
+          DisplayWord(SIX);
           break;
         case 7: 
-          DisplayWord(SEVEN, "Seven");
+          DisplayWord(SEVEN);
           break;
         case 8: 
-          DisplayWord(EIGHT, "Eight"); 
+          DisplayWord(EIGHT); 
           break;
         case 9: 
-          DisplayWord(NINE, "Nine");
+          DisplayWord(NINE);
           break;
         case 10: 
-          DisplayWord(HTEN, "Ten"); 
+          DisplayWord(HTEN); 
           break;
         case 11: 
-          DisplayWord(ELEVEN, "Eleven"); 
+          DisplayWord(ELEVEN); 
           break;
         case 12: 
-          DisplayWord(TWELVE, "Twelve");
+          DisplayWord(TWELVE);
           break;
          case 13:
-          DisplayWord(ONE, "One");
+          DisplayWord(ONE);
           break;
     }
 }
@@ -287,61 +235,61 @@ void displaytime(void){
   digitalClockDisplay();
   ledsoff();
   
-  DisplayWord(ITIS, "It Is");
+  DisplayWord(ITIS);
 
   // now we display the appropriate minute counter
   if ((minute()>4) && (minute()<10)) { 
-    DisplayWord(MFIVE, "Five");
+    DisplayWord(MFIVE);
   } 
   if ((minute()>9) && (minute()<15)) { 
-    DisplayWord(MTEN, "Ten");
+    DisplayWord(MTEN);
   }
   if ((minute()>14) && (minute()<20)) {
-    DisplayWord(QUARTER, "Quarter"); 
+    DisplayWord(QUARTER); 
   }
   if ((minute()>19) && (minute()<25)) { 
-    DisplayWord(TWENTY,"Twenty");
+    DisplayWord(TWENTY);
   }
   if ((minute()>24) && (minute()<30)) { 
-    DisplayWord(TWENTY, "Twenty");
-    DisplayWord(MFIVE, "Five");
+    DisplayWord(TWENTY);
+    DisplayWord(MFIVE);
   }  
   if ((minute()>29) && (minute()<35)) {
-    DisplayWord(HALF, "Half");
+    DisplayWord(HALF);
   }
   if ((minute()>34) && (minute()<40)) { 
-    DisplayWord(TWENTY, "Twenty"); 
-    DisplayWord(MFIVE, "Five"); 
+    DisplayWord(TWENTY); 
+    DisplayWord(MFIVE); 
   }  
   if ((minute()>39) && (minute()<45)) { 
-    DisplayWord(TWENTY, "Twenty");
+    DisplayWord(TWENTY);
   }
   if ((minute()>44) && (minute()<50)) {
-    DisplayWord(QUARTER, "Quarter");
+    DisplayWord(QUARTER);
   }
   if ((minute()>49) && (minute()<55)) { 
-    DisplayWord(MTEN, "Ten");
+    DisplayWord(MTEN);
   } 
   if (minute()>54) { 
-    DisplayWord(MFIVE, "Five");
+    DisplayWord(MFIVE);
   }
 
   if ((minute()<5))
   {
     displayHour(0);
-    DisplayWord(OCLOCK, "O'Clock");
+    DisplayWord(OCLOCK);
   }
   else
     if ((minute() < 35) && (minute() >4))
     {
-      DisplayWord(PAST, "Past");
+      DisplayWord(PAST);
       displayHour(0);
     }
     else
     {
       // if we are greater than 34 minutes past the hour then display
       // the next hour, as we will be displaying a 'to' sign
-      DisplayWord(TO, "To");
+      DisplayWord(TO);
       displayHour(1);
     }
     
@@ -350,53 +298,52 @@ void displaytime(void){
       case 0:
         break;
       case 1:
-        DisplayWord(MIN1, ".");
+        DisplayWord(MIN1);
         break;
       case 2:
-        DisplayWord(MIN1, ".");
-        DisplayWord(MIN2, ".");
+        DisplayWord(MIN1);
+        DisplayWord(MIN2);
         break;
       case 3:
-        DisplayWord(MIN1, ".");
-        DisplayWord(MIN2, ".");
-        DisplayWord(MIN3, ".");
+        DisplayWord(MIN1);
+        DisplayWord(MIN2);
+        DisplayWord(MIN3);
         break;
       case 4:
-        DisplayWord(MIN1, ".");
-        DisplayWord(MIN2, ".");
-        DisplayWord(MIN3, ".");
-        DisplayWord(MIN4, ".");
+        DisplayWord(MIN1);
+        DisplayWord(MIN2);
+        DisplayWord(MIN3);
+        DisplayWord(MIN4);
         break;
    }
    strip.show();
+   Serial.println();
 }
 
 
-/*
 void decreaseBrightness(void) {
    //if brightness isn't on lowest setting, cut in half. 
    //otherwise return to full brightness
    if (brightness == 1) {
-     brightness = 255;
+     brightness = MAX_BRIGHTNESS;
    } else {
      brightness = int(brightness/2);
    }
-   analogWrite(PWMPin, brightness);
-   Serial.println("Brightness set to ");
+   strip.setBrightness(brightness);
+   Serial.print(F("Brightness set to "));
    Serial.println(brightness);
  }
- */
  
 void checkHourButton() {
    //set hours based on hour button behavior
    
-    if (digitalRead(HourButtonPin) == 0 && hourButtonDown == 0) {
+    if (digitalRead(HourPin) == 0 && hourButtonDown == 0) {
       //hour button pushed
       hourButtonDown = 1; //hour button is being pressed
       pushStart = millis(); //remember time that button was first pressed
     }
     
-    if (digitalRead(HourButtonPin) == 1 && hourButtonDown == 1) {
+    if (digitalRead(HourPin) == 1 && hourButtonDown == 1) {
       //hour button released: if released from a long press, do nothing.
       //if released from a 'short' press, increase the hour.
       if (!inLongPress) {
@@ -424,13 +371,13 @@ void checkMinuteButton() {
   
   //set minutes based on minute button behavior
   
-  if (digitalRead(MinuteButtonPin) == 0 && minuteButtonDown == 0) {
+  if (digitalRead(MinutePin) == 0 && minuteButtonDown == 0) {
     //minute button pushed
     minuteButtonDown = 1; //minute button is being pressed
     pushStart = millis(); //mark time that button was pushed
   }
   
-  if (digitalRead(MinuteButtonPin) == 1 && minuteButtonDown == 1) {
+  if (digitalRead(MinutePin) == 1 && minuteButtonDown == 1) {
     //minute button released
     if (!inLongPress) {
       adjustTime(60);
@@ -454,40 +401,38 @@ void checkMinuteButton() {
   }  
 }
 
-/*
 void checkBrightnessButton() {
   //set brightness based on brightness button behavior
-  if (digitalRead(BrightnessButtonPin) == 0) {
+  if (digitalRead(BrightnessPin) == 0) {
     brightnessButtonDown = 1;
   }
-  if (digitalRead(BrightnessButtonPin) == 1 && brightnessButtonDown == 1) {
+  if (digitalRead(BrightnessPin) == 1 && brightnessButtonDown == 1) {
     brightnessButtonDown = 0;
-    Serial.println("Brightness button released");
+    Serial.println(F("Brightness button released"));
     decreaseBrightness();
     displaytime();
   }
 }
-*/
 
 void digitalClockDisplay(){
   // digital clock display of the time
   Serial.print(hour());
   printDigits(minute());
   printDigits(second());
-  Serial.print(" ");
+  Serial.print(F(" "));
   Serial.print(day());
-  Serial.print(" ");
+  Serial.print(F(" "));
   Serial.print(month());
-  Serial.print(" ");
+  Serial.print(F(" "));
   Serial.print(year()); 
   Serial.println(); 
 }
 
 void printDigits(int digits){
   // utility function for digital clock display: prints preceding colon and leading 0
-  Serial.print(":");
+  Serial.print(F(":"));
   if(digits < 10)
-    Serial.print('0');
+    Serial.print(F("0"));
   Serial.print(digits);
 }
 
